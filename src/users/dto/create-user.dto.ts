c68e76267a5c3a92fd7e5892c42616355dd3a59b
trak2026z <trak2026z@gmail.com>
2025-08-13 01:29:43 +0200

    feat: add create-user.dto.ts

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,12 @@
+import { IsEmail, IsNotEmpty, IsString, MaxLength } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(100)
+  name: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+}
