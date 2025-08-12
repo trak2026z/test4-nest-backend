16b208c719a06c8754a5978e319c5bb7334952ed
trak2026z <trak2026z@gmail.com>
2025-08-13 01:30:24 +0200

    feat: add users.service.ts

---
+++ src/users/users.service.ts
@@ -0,0 +1,28 @@
+import { Injectable, ConflictException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
+import { CreateUserDto } from './dto/create-user.dto';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
+
+  async findAll(): Promise<User[]> {
+    return this.usersRepository.find();
+  }
+
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    const existing = await this.usersRepository.findOne({
+      where: { email: createUserDto.email },
+    });
+    if (existing) {
+      throw new ConflictException('Email already exists');
+    }
+    const user = this.usersRepository.create(createUserDto);
+    return this.usersRepository.save(user);
+  }
+}
