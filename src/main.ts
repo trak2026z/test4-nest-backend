386910a0b52da569788fe65345e87d5de1a924a7
trak2026z <trak2026z@gmail.com>
2025-08-13 01:27:37 +0200

    feat: add main.ts (NestJS bootstrap)

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+
+async function bootstrap() {
+  dotenv.config();
+  const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
+
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  console.log(`🚀 Server running on http://localhost:${port}`);
+}
+bootstrap();
