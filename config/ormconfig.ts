8043455cc1ad2b26271b3c55366c50d554c55426
trak2026z <trak2026z@gmail.com>
2025-08-13 17:53:22 +0200

    fix: correct parentheses in DataSource initialization

--- config/ormconfig.ts
+++ config/ormconfig.ts
@@ -5,29 +5,32 @@ const isEnvProd = process.env.NODE_ENV === 'production';
 
 const entitiesPath = path.join(__dirname, '../src/***.entity{.nts,.js}');
 const migrationsPath = path.join(__dirname, isEnvProd
-    ? '../dist/src/migrations/*.js'
-    : '../src/migrations/*.n{ts,js}');
+  isEnvProd ? '../dist/src/migrations/*.js' : '../src/migrations/*{ts,.js}',
+);
 
 export const AppDataSource = process.env.DATABASE_URL
-    ? (new DataSource({
-        type: 'postgres',
-        url: process.env.DATABASE_URL,
-        ssl: isEnvProd ? { rejectUnauthorized: false } : false,
-        entities: [entitiesPath],
-        migrations: [migrationsPath],
-        synchronize: false,
-    })
-    : new DataSource({type: 'postgres',
-        host: process.env.DB_HOST,
-        port: Number(process.env.DB_PORT) || 5432,
-        username: process.env.DB_USERNAME,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-        entities: [entitiesPath],
-        migrations: [migrationsPath],
-        synchronize: false,
-        ssl: isEnvProd ? { rejectUnauthorized: false } : false,
-    }));
+  ? new DataSource({
+    type: 'postgres',
+    url: process.env.DATABASE_URL,
+    ssl: setSsl(),
+    entities: [entitiesPath],
+    migrations: [migrationsPath],
+    synchronize: false,
+})
+  : new DataSource({type: 'postgres',
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT) || 5432,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    entities: [entitiesPath],
+    migrations: [migrationsPath],
+    synchronize: false,
+    ssl: setSsl(),
+});
 
-// Default export for import v aplikacji zA`p
+function setSsl() {
+  return isEnvProd ? { rejectUnauthorized: false } : false;
+}
+
 export default AppDataSource;
