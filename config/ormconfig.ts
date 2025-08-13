b140c85b3db47210880873964ee535d3ef7bb1f9
trak2026z <trak2026z@gmail.com>
2025-08-13 17:56:09 +0200

    fix: correct ternary in migrationsPath to avoid boolean param to path.join

--- config/ormconfig.ts
+++ config/ormconfig.ts
@@ -4,30 +4,28 @@ import * as path from 'path';
 const isEnvProd = process.env.NODE_ENV === 'production';
 
 const entitiesPath = path.join(__dirname, '../src/***.entity{.nts,.js}');
-const migrationsPath = path.join(__dirname, isEnvProd
-  isEnvProd ? '../dist/src/migrations/*.js' : '../src/migrations/*{ts,.js}',
-);
+const migrationsPath = path.join(__dirname, isEnvProd ? '../dist/src/migrations/*.js' : '../src/migrations/*{ts,.js}');
 
 export const AppDataSource = process.env.DATABASE_URL
   ? new DataSource({
-    type: 'postgres',
-    url: process.env.DATABASE_URL,
-    ssl: setSsl(),
-    entities: [entitiesPath],
-    migrations: [migrationsPath],
-    synchronize: false,
-})
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      ssl: setSsl(),
+      entities: [entitiesPath],
+      migrations: [migrationsPath],
+      synchronize: false,
+  })
   : new DataSource({type: 'postgres',
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT) || 5432,
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    entities: [entitiesPath],
-    migrations: [migrationsPath],
-    synchronize: false,
-    ssl: setSsl(),
-});
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT) || 5432,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORG,
+      database: process.env.DB_NAME,
+      entities: [entitiesPath],
+      migrations: [migrationsPath],
+      synchronize: false,
+      ssl: setSsl(),
+  });
 
 function setSsl() {
   return isEnvProd ? { rejectUnauthorized: false } : false;
