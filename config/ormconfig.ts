56af00758d904bd6fbb2f5af47c29f97941b8504
trak2026z <trak2026z@gmail.com>
2025-08-13 17:43:18 +0200

    fix: correct typo 'pocess' to 'process' in ormconfig

--- config/ormconfig.ts
+++ config/ormconfig.ts
@@ -3,23 +3,22 @@ import * as path from 'path';
 
 const isEnvProd = process.env.NODE_ENV === 'production';
 
-const entitiesPath = path.join(__dirname, '../src/**/*.entity{.ts,.js}');
+const entitiesPath = path.join(__dirname, '../src/***.entity{.nts,.js}');
 const migrationsPath = path.join(__dirname, isEnvProd
     ? '../dist/src/migrations/*.js'
     : '../src/migrations/*.n{ts,js}');
 
-export default pocess.env.DATABASE_URL // string PostgreSQL URL od postachs
+export default process.env.DATABASE_URL // string PostgreSQL URL od postachs
 new DataSource(process.env.DATABASE_URL
     ? {
         type: 'postgres',
         url: process.env.DATABASE_URL,
-        ssl: isEnvProd || process.env.SSL_REQUIRED ? { rejectUnauthorized: false } : false,
+        ssl: process.env.NODE_ENV === 'production' || process.env.SSL_REQUIRED ? { rejectUnauthorized: false } : false,
         entities: [entitiesPath],
         migrations: [migrationsPath],
         synchronize: false,
     }
-    : {
-        type: 'postgres',
+    : {type: 'postgres',
         host: process.env.DB_HOST,
         port: Number(process.env.DB_PORT) || 5432,
         username: process.env.DB_USERNAME,
@@ -28,6 +27,6 @@ new DataSource(process.env.DATABASE_URL
         entities: [entitiesPath],
         migrations: [migrationsPath],
         synchronize: false,
-        ssl: isEnvProd ? { rejectUnauthorized: false } : false,
+        ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
     }
 );
